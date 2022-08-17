c52abe3d2694dcdb6fd9b727b1c73eb1543f5ee4
krist7599555 <krist7599555@gmail.com>
2022-08-17 15:41:02 +0700

    feat: build basic file

---
+++ dist/vite-plugin-child-process.d.ts
@@ -0,0 +1,15 @@
+import { type Plugin } from "vite";
+export interface VitePluginChildProcess {
+    /** command name for prefix log ouput @default "untitle" */
+    name?: string;
+    /** command to run */
+    command: string[];
+    /** @default 10ms */
+    delay?: number;
+    /** vite watch file change */
+    watch: (RegExp | string)[];
+    /** @default true */
+    log_enable?: boolean;
+}
+export declare const child_process: (config: VitePluginChildProcess) => Plugin;
+//# sourceMappingURL=vite-plugin-child-process.d.ts.map

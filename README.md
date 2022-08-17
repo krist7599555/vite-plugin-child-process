559b2b33129699da1f8ec064bb2e1f8b77227d43
krist7599555 <krist7599555@gmail.com>
2022-08-17 15:23:50 +0700

    feat: init

---
+++ README.md
@@ -0,0 +1,47 @@
+# vite-plugin-child-process
+
+Vite plugin to run command when file change. Useful when script use local binary or have stateful behavior like websocket.
+
+```typescript
+// vite.config.js
+import { child_process } from 'vite-plugin-child-process'
+
+export default {
+  plugins: [
+    child_process({
+      name: "my-backend-server",
+      command: ["node", "./backend_server.js"],
+      watch: [/src/, /controller/]
+    })
+  ]
+};
+```
+
+## Parameters
+
+```typescript
+export interface VitePluginChildProcess {
+  /** command name for prefix log ouput @default "untitle" */
+  name?: string;
+  /** command to run */
+  command: string[];
+  /** @default 10ms */
+  delay?: number;
+  /** vite watch file change */
+  watch: (RegExp | string)[];
+  /** @default true */
+  log_enable?: boolean;
+}
+```
+
+## Environment
+
+this plugin use `debug` module to log, specify environment `DEBUG=` will show stdout/stderr of command. 
+
+```bash
+DEBUG=vite:child-process:*
+```
+
+## Footnote
+
+PR and issue always wellcome
---
+++ lib/core.ts
@@ -0,0 +1,92 @@
+import debug from "debug";
+import path from "node:path";
+import { Stream } from "node:stream";
+import {
+  debounce,
+  filter,
+  interval,
+  map,
+  Observable,
+  startWith,
+  tap,
+} from "rxjs";
+import { type Plugin } from "vite";
+import { type ProcessPromise, $ } from "zx";
+
+export interface VitePluginChildProcess {
+  /** command name for prefix log ouput @default "untitle" */
+  name?: string;
+  /** command to run */
+  command: string[];
+  /** @default 10ms */
+  delay?: number;
+  /** vite watch file change */
+  watch: (RegExp | string)[];
+  /** @default true */
+  log_enable?: boolean;
+}
+
+export const child_process = (config: VitePluginChildProcess): Plugin => {
+  const _command = config.command;
+  const _delay = config?.delay ?? 10;
+  const _watch = config?.watch.map((w) =>
+    typeof w == "string" ? new RegExp(w) : w
+  );
+
+  const log = debug(`vite:child-process:${config.name ?? "untitle"}`);
+  log.enabled = config?.log_enable ?? true;
+
+  const log_watcher = log.extend("watcher");
+
+  const old_process: ProcessPromise[] = [];
+  async function kill_all_process() {
+    for (const ps of old_process) {
+      await ps.kill();
+      await ps.exitCode;
+    }
+  }
+
+  return {
+    name: "vite-plugin-vite-child-process",
+    enforce: "post",
+    apply: "serve",
+    async closeBundle() {
+      log_watcher("close bundle");
+      await kill_all_process();
+    },
+    async configureServer({ watcher }) {
+      log_watcher("start configure server");
+      log_watcher(
+        "watcher file change",
+        JSON.stringify(_watch.map((r) => `${r}`))
+      );
+      const files = new Observable<string>((o) => {
+        watcher.on("ready", () => o.next(""));
+        watcher.on("change", (id) => o.next(id));
+        watcher.on("add", (id) => o.next(id));
+      });
+
+      const filechange$ = files.pipe(
+        filter((id) => _watch.some((regex) => regex.test(id))),
+        map((fullpath) => path.relative(path.resolve(), fullpath)), // change to realtive
+        debounce(() => interval(_delay)),
+        tap((id) => log_watcher("filechange " + id)),
+        startWith(null)
+      );
+
+      filechange$.subscribe(async () => {
+        await kill_all_process();
+        const local_process: ProcessPromise = $`${_command}`;
+        old_process.push(local_process);
+        local_process.quiet();
+
+        local_process.stdout.on("data", (s: Stream) =>
+          log(s.toString().trimEnd())
+        );
+        local_process.stdout.on("error", (s: Stream) =>
+          log(s.toString().trimEnd())
+        );
+      });
+    },
+  };
+};
